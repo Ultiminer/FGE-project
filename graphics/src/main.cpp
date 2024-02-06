38b162bb74443e42e73ec2b1b5629bb6c08ecd5d
FloDev <43553244+Ultiminer@users.noreply.github.com>
2024-02-06 13:07:44 +0100

    Image support temporarily removed due to buggy behaviour!

--- graphics/src/main.cpp
+++ graphics/src/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include "math.h"
 #include "FGE_setup.h"
-#include "FGE_image.h"
 
 //This library can be super extended with any kind of SDL, SDL2, or OpenGL functionality 
 int FGE_Main()
@@ -11,10 +10,8 @@ int FGE_Main()
     FGE::Window wind= FGE_General_Init();
     
     FGE::SSquircle rect={0,0,100,100};
-    FGE_Texture text=FGE_Texture("good.bmp");
     FGE_Loop_Start(wind)
         rect.Draw({yellow|brown}).DrawBorder();
-        FGE_DrawTexture(text,{20,10,100,100});
     FGE_Loop_End(wind)
     
 
